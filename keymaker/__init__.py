d89be26f1741f63b624157180dbd373287f6e893
Michael Rollins <39917923+upskill-mrollins@users.noreply.github.com>
2018-09-04 10:03:49 -0500

    Fix cronjob
    "root" is not an executable.  Removing it fixes it.

--- keymaker/__init__.py
+++ keymaker/__init__.py
@@ -272,7 +272,7 @@ def install(args):
             print(line, file=fh)
 
     with open("/etc/cron.d/keymaker-group-sync", "w") as fh:
-        print("*/5 * * * * root /usr/local/bin/keymaker sync_groups", file=fh)
+        print("*/5 * * * * /usr/local/bin/keymaker sync_groups", file=fh)
 
 def err_exit(msg, code=3):
     print(msg, file=sys.stderr)
